7bccaa74b4120ddc7bd3ff825167091068a1e9ba
Victor Mota <victor.vmt@outlook.com>
2020-02-01 12:21:01 -0300

    [WordCount] Ordenando resultados

--- src/components/WordCount/WordCount.js
+++ src/components/WordCount/WordCount.js
@@ -3,14 +3,18 @@ import './WordCount.css';
 
 export default function WordCount(props) {
 
-    if (props.words.length)
+    if (props.words.length) {
 
         return (<div className="word-count">
-            {props.words.map(word => <div key={word.value} className="word-count-group">
+            {props.words.sort(((a, b) => {
+                if (a.count < b.count) return 1;
+                else if (a.count > b.count) return -1;
+                else return 0;
+            })).map(word => (<div key={word.value} className="word-count-group">
                 <span className="word-count-title">{word.value}</span>
                 <span className="word-count-subtitle">{word.count} repetições</span>
-            </div>)}
+            </div>))}
         </div>);
 
-    else return null;
+    } else return null;
 }
