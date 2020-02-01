b97d60bb8946a945ac919fd84a95cff867d1e544
Victor Mota <victor.vmt@outlook.com>
2020-02-01 11:06:22 -0300

    Criando estrutura inicial da interface

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Input, Icon} from "antd";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <header className="header">Essay Analyzer</header>
+            <main className="content">
+                <div className="container">
+                    <div className="input-text-container">
+                        <Input.TextArea/>
+                    </div>
+                </div>
+            </main>
+            <footer className="footer"><a href="https://github.com/victormt4/essay-analyzer" target="_blank"><Icon type="github"/> Repo</a>
+            </footer>
+        </>
+    );
 }
 
 export default App;
